b24f5d5c3215c1f35403a559cbc3581e59ee12ac
aritr <aritr@DESKTOP-6EUNS17>
2022-05-31 15:17:53 +0530

    first commit

---
+++ Scanner Class/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	//String name =sc.nextInt();
+	int age =sc.nextInt();
+	//char gender =sc.next();
+	long mobileno =sc.nextInt();
+	double cgpa=sc.nextDouble();
+			
+//	System.out.println("name" +name);
+	System.out.println("age" +" "+age);
+//	System.out.println("gender"+gender);
+	System.out.println("mobileno" +" "+mobileno);
+	System.out.println("cgpa" +" "+cgpa);
+}
+}
