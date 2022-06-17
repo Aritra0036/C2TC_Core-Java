14a53a36402ff3cebb536ccf7df200345063dbfa
aritr <aritr@DESKTOP-6EUNS17>
2022-06-17 15:41:04 +0530

    first commit

---
+++ interface/src/interfaceexample.java
@@ -0,0 +1,12 @@
+
+public class interfaceexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	}
+
+}
+//interface can not  implements interface 
+//interface extends interface only
