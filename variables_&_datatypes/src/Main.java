e3094c44848692a0fc679cc1d711e376ed7a26e4
aritr <aritr@DESKTOP-6EUNS17>
2022-06-17 15:46:43 +0530

    first commit

---
+++ variables_&_datatypes/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int myInt=9;
+		double myDouble = myInt;
+		System.out.println(myDouble);
+		System.out.println(myInt);
+
+	}
+
+}
---
+++ variables_&_datatypes/src/test.java
@@ -0,0 +1,21 @@
+
+public class test {
+	static void test(float x) {
+		System.out.println("float");
+       }
+	static void test(double x) {
+		System.out.println("double");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test(99.9);
+		int i=010;
+		int j=07;
+		int z=10;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(z);
+	}
+
+}
