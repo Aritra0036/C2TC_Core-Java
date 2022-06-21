682ca479fb6e9d85e7f86f6863078d13573eae8b
aritr <aritr@DESKTOP-6EUNS17>
2022-06-21 15:49:46 +0530

    first commit

---
+++ collection/src/com/tns/collection/Main.java
@@ -0,0 +1,20 @@
+package com.tns.collection;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List <Integer>  I=new ArrayList<Integer>();
+		I.add(1);
+		I.add(2);
+		I.add(3);
+		I.add(4);
+		I.add(5);
+		I.add(6);
+		I.add(7);
+		System.out.println(I);
+		
+	}
+
+}
